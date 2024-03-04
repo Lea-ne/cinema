d8421f2418a4ed96ae8fbb2f9da57ee5608ffb42
Leane <leaneleboulanger@gmail.com>
2024-03-04 18:11:18 +0000

    Ajout du trailer video

--- cinema/src/components/movieDetail/index.js
+++ cinema/src/components/movieDetail/index.js
@@ -1,8 +1,30 @@
 import React, { useEffect, useState } from "react";
+import YouTube from 'react-youtube';
 
 export default function MovieDetail(props) {
   const [movieDetails, setMovieDetails] = useState(null);
   const [showAllActors, setShowAllActors] = useState(false);
+  const [trailerVideoId, setTrailerVideoId] = useState(null);
+
+
+
+  //fonction
+  const extractTrailerVideoId = (videoResults) => {
+    if (videoResults && videoResults.length > 0) {
+      const trailerVideo = videoResults.find((video) => video.type === "Trailer");
+      return trailerVideo ? trailerVideo.key : null;
+    }
+    return null;
+  };
+
+  console.log(movieDetails);
+
+  const handleSeeMoreClick = () => {
+    setShowAllActors(!showAllActors);
+  };
+
+
+
 
   //lien avec api
   useEffect(() => {
@@ -10,23 +32,22 @@ export default function MovieDetail(props) {
     if (props.movie) {
       // Récupérez les détails du film depuis l'API TMDb
       fetch(
-        `https://api.themoviedb.org/3/movie/${props.movie.id}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&append_to_response=credits`
+        `https://api.themoviedb.org/3/movie/${props.movie.id}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&append_to_response=credits,videos`
       )
         .then((response) => response.json())
         .then((data) => {
           setMovieDetails(data);
+
+          const trailerVideoId = extractTrailerVideoId(data.videos.results);
+          setTrailerVideoId(trailerVideoId);
         })
         .catch((error) => console.error("Error fetching movie details:", error));
     }
   }, [props.movie]);
 
 
-  //fonction
-  console.log(movieDetails);
 
-  const handleSeeMoreClick = () => {
-    setShowAllActors(!showAllActors);
-  };
+
 
 
 
@@ -70,7 +91,7 @@ export default function MovieDetail(props) {
 
             {movieDetails.poster_path && (<img src={`https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`} alt={movieDetails.title} />)}
 
-            <iframe src={`https://video.tmdb.org/${movieDetails.videos}`}></iframe>
+            {trailerVideoId && <YouTube videoId={trailerVideoId} />}
 
         </div>
       ) : (
